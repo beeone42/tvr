2b832fc252b6b9359225e4b5fb71be1bc1739aac
sb <sb@staff.42.fr>
2015-04-15 16:39:46 +0200

    templates

--- handlers.go
+++ handlers.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-    "fmt"
+    "html/template"
     "net/http"
 )
 
+func renderTemplate(w http.ResponseWriter, tmpl string, title string) {
+    t, err := template.ParseFiles("views/" + tmpl + ".html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    err = t.Execute(w, title)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path)
+    title := r.URL.Path[len("/"):]
+	renderTemplate(w, "main", title)
 }
 
 func ajaxHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love AJAX %s!", r.URL.Path)
 }
 
