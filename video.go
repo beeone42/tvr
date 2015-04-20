a792261fcda22a77d0174a230fcb7ddceb4fde74
jacob <jacob@jacobs-iMac.local>
2015-04-20 19:11:53 +0200

    je casse tout

--- video.go
+++ video.go
@@ -8,7 +8,7 @@ import (
 
 
 func listVideo() ([]string, error) {
-	res, err := filepath.Glob(path.Join("video", "*.mp4"))
+	res, err := filepath.Glob(path.Join("video", "*.mp4, *.MP4"))
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
